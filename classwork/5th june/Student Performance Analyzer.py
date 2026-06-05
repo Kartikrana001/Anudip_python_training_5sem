d6955bbd6f7c5d6f83d9655162b7f5e3a247eacc
Kartikrana001 <kr3511441@gmail.com>
2026-06-05 14:34:20 +0530

    Create Student Performance Analyzer.py

---
+++ classwork/5th june/Student Performance Analyzer.py
@@ -0,0 +1,29 @@
+marks = [78, 45, 92, 35, 88, 40, 99, 56]
+
+passed = []
+failed = 0
+merit = []
+
+highest = marks[0]
+lowest = marks[0]
+
+for m in marks:
+    if m >= 40:
+        passed.append(m)
+    else:
+        failed += 1
+
+    if m > highest:
+        highest = m
+
+    if m < lowest:
+        lowest = m
+
+    if m > 75:
+        merit.append(m)
+
+print("Passed Students:", passed)
+print("Failed Count:", failed)
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
+print("Merit List:", merit)
