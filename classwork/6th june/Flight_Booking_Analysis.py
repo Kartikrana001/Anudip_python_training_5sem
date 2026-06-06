b48b123942acd3b53ab17d8d848d07f2446761b5
Kartikrana001 <kr3511441@gmail.com>
2026-06-06 10:38:11 +0530

    Update Flight_Booking_Analysis.py

--- classwork/6th june/Flight_Booking_Analysis.py
+++ classwork/6th june/Flight_Booking_Analysis.py
@@ -19,33 +19,31 @@ cancelled = 0
 
 waiting_list = []
 
-destinations = {}
+delhi = 0
+mumbai = 0
+chennai = 0
 
 for b in bookings:
 
     if b[1] == "Delhi":
         delhi_count += 1
+        delhi += 1
+
+    elif b[1] == "Mumbai":
+        mumbai += 1
+
+    elif b[1] == "Chennai":
+        chennai += 1
 
     if b[2] == "Confirmed":
         confirmed += 1
+
     elif b[2] == "Waiting":
         waiting += 1
         waiting_list.append(b[0])
+
     elif b[2] == "Cancelled":
         cancelled += 1
-
-    if b[1] in destinations:
-        destinations[b[1]] += 1
-    else:
-        destinations[b[1]] = 1
-
-most_booked = ""
-max_count = 0
-
-for city in destinations:
-    if destinations[city] > max_count:
-        max_count = destinations[city]
-        most_booked = city
 
 print("\nPassengers Travelling to Delhi:", delhi_count)
 
@@ -57,4 +55,12 @@ print("\nWaiting List:")
 print(waiting_list)
 
 print("\nMost Booked Destination:")
-print(most_booked, max_count)
+
+if delhi >= mumbai and delhi >= chennai:
+    print("Delhi", delhi)
+
+elif mumbai >= delhi and mumbai >= chennai:
+    print("Mumbai", mumbai)
+
+else:
+    print("Chennai", chennai)
